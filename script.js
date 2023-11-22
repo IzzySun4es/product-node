2448d974976819247129025d4a5acdf32643520b
Alexandra Chugaenko <sascha281201@gmain.com>
2023-11-22 13:11:17 +0300

    1

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const urlparser = bodyParser.urlencoded({extended: false});
+
+const Sequelize = require('sequelize');
 const app = express();
+
+
+const sequelize = new Sequelize("shop", "root", "", {
+    dialect: 'mysql',
+    host: 'localhost',
+});
+
+
+const Products = sequelize.define("products", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false
+    },
+
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    count: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    },
+});
 
 app.set("view engine", "hbs");
 
@@ -9,16 +42,44 @@ let products = [
     {id: 3, name: 'сок', count: 16, price: 60},
 ];
 
-app.get('/products.hbs', function (req, res){
-    res.render("products.hbs", {products: products})
+app.get('/products', function (req, res){
+
+    Products.findAll({raw: true}).then((data) => {
+        res.render("products.hbs", {products: data});
+    });
+
+})
+
+app.post('/products', urlparser, function (req, res){
+
+    const name =req.body.name;
+    const count =req.body.count;
+    const price =req.body.price;
+
+    console.log(req.body);
+
+    Products.create({name: name, count: count, price: price}).then(() => {
+        res.redirect('/products');
+    })
+    
+
+
 })
 
 app.get('/product/:id', function (req, res){
     const productId = req.params.id
-    res.render("product.hbs", {product: products[productId-1]})
-})
+
+    Products.findOne({where: { id: productId }}).then((data) => {
+        res.render("product.hbs", {product: data});
+    })
+    
+});
 
 app.get('/', function (req, res){
     res.render("index.hbs", {name: 'Alica'})
 });
-app.listen(3000)
+
+sequelize.sync().then(() => {
+    app.listen('3000');
+})
+
